339c452c1223e6d7824fe44c7739eb60bb74ab12
Tetreos <bomaxim7@gmail.com>
2022-12-09 20:36:13 +0100

    Clean up

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// getting all required files
-require('./deploy-commands.js');
+//require('./deploy-commands.js');
+
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, GatewayIntentBits, ActivityType, Events, Collection } = require('discord.js');
 const { token } = require('./config.json');
 const prefix = '!'
 
-// setup client
 const client = new Client({ 
   intents: [
     GatewayIntentBits.Guilds,
@@ -23,7 +22,7 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
+
 	if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
 	} else {
@@ -56,19 +55,15 @@ client.once(Events.ClientReady, c => {
 });
 
 client.on('messageCreate', async (message) => {
-	if (checkCommand(message, 'help')) {
-	const reply = await message.reply({ content : 'You triggered the help command!', allowedMentions : { parse: []}})
-	}
-	if (checkCommand(message, 'ping')) {
-	const reply = await message.channel.send(`Latency is ${message.createdTimestamp - Date.now()}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-	}
-	if (checkCommand(message, 'test')) {
-	const reply = await message.reply({ embeds : [exampleEmbed] })
+	if (message.content.toLowerCase().startsWith(prefix)) {
+		try {
+			const command = client.commands.get(message.content.slice(1));
+			command.execute(message);
+		} catch (error) {
+			console.error(error);
+			await message.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
 	}
 });
 
 client.login(token);
-
-function checkCommand(message, commandName) {
-  return message.content.toLowerCase().startsWith(prefix + commandName);
-}
