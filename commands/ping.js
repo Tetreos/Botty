903124dd724dbdc0ba262636ffe21413923b8633
Tetreos <bomaxim7@gmail.com>
2022-12-09 20:35:23 +0100

    Rewritten Command Handler

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,12 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply(`Latency is ${interaction.createdTimestamp - Date.now()}ms. API Latency is ${Math.round(interaction.client.ws.ping)}ms`);
+	async execute(i) {
+		if (i.type === 2) {
+			await i.reply(`Latency is ${i.createdTimestamp - Date.now()}ms. API Latency is ${Math.round(i.client.ws.ping)}ms`);
+		}
+		else {
+			await i.reply(`Latency is ${i.createdTimestamp - Date.now()}ms. API Latency is ${Math.round(i.client.ws.ping)}ms`);
+		}
 	},
 };
--- commands/server.js
+++ commands/server.js
@@ -4,8 +4,12 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
 		.setDescription('Provides information about the server.'),
-	async execute(interaction) {
-		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+	async execute(i) {
+		if (i.type === 2) {
+			await i.reply(`This server is ${i.guild.name} and has ${i.guild.memberCount} members.`);
+		}
+		else {
+			await i.reply(`This server is ${i.guild.name} and has ${i.guild.memberCount} members.`);
+		}
 	},
 };
