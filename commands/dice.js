9dbfa5a31eeeab33fd846b247a44408bdca36d0b
Tetreos <bomaxim7@gmail.com>
2023-03-12 13:26:53 +0100

    Enhanced dice function

--- commands/dice.js
+++ commands/dice.js
@@ -16,21 +16,24 @@ module.exports = {
         const dice = i.options.getString('dice');
         const sides = i.options.getString('sides');
 
+        const Embed = new EmbedBuilder()
+		.setAuthor({
+			name: `${i.user.username} throws ${dice}d${sides}`,
+			iconURL: i.user.avatarURL(),
+		})
+
         if(isNaN(dice) || isNaN(sides) || dice <= 0 || sides <= 0) {
             return i.reply('Only positive integers are allowed!');
         }
 
         let result = 0
         for(let i = 0; i < dice; i++) {
-            result += Math.floor(Math.random() * sides) + 1;
+            let tmp = Math.floor(Math.random() * sides) + 1;
+            result += tmp;
+            Embed.addFields({name: `Dice ${i + 1}`, value: `${tmp}`, inline: true});
         }
 
-		const Embed = new EmbedBuilder()
-		.setAuthor({
-			name: `${i.user.username} throws ${dice}d${sides}`,
-			iconURL: i.user.avatarURL(),
-		})
-		.setDescription(`Result is ${result}`)
+		Embed.addFields({name: `Total dice throw`, value: `${result}`, inline: false})
 		await i.reply({ embeds: [Embed] });
 	},
 };
