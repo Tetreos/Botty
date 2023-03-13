b5f083627cbc27d4192c71163cbda46d03e2f65f
Tetreos <bomaxim7@gmail.com>
2023-03-13 09:17:10 +0100

    fixed dice

--- commands/dice.js
+++ commands/dice.js
@@ -22,7 +22,7 @@ module.exports = {
 			iconURL: i.user.avatarURL(),
 		})
 
-        if(!isInteger(dice) || !isInteger(sides) || dice <= 0 || sides <= 0) {
+        if(!Number.isInteger(dice) || !Number.isInteger(sides) || dice <= 0 || sides <= 0) {
             return i.reply('Only positive integers are allowed!');
         }
 
